c90c2c2d2759de47d8b40371175854e132428f8e
Bjørn André Aaslund <BjornAndre@dhcp-10-22-72-39.wlan.ntnu.no>
2020-10-15 11:34:21 +0200

    latex tables for describe

--- Source/visualization/data_exploration.py
+++ Source/visualization/data_exploration.py
@@ -6,58 +6,68 @@ import seaborn as sns
 
 # General Settings
 loaddir = "../../Data/"
-savedir = "../../Log/"
-if not os.path.exists(savedir):
-    os.mkdir(savedir)
+savedir_log = "../../Log/"
+if not os.path.exists(savedir_log):
+    os.mkdir(savedir_meta)
+savedir_meta = "../../Data/meta/"
+if not os.path.exists(savedir_meta):
+    os.mkdir(savedir_meta)
+
 
 train = pd.read_csv(loaddir + "raw/train.csv", header=0)
-print(train.columns)
 
-# season has 4 unique values
-print(train['season_x'].unique())
-# has 12 unique values
-print(train['month_x'].unique())
-# has
-print(len(train['hour_x'].unique()))
+# %% ---------------------- Removing prophet values and _y values --------------------- #
+df = train[['demand', 'grid1-load', 'grid1-loss', 'grid1-temp', 'grid2-load', 'grid2-loss', 'grid2_1-temp', 'grid2_2-temp', 'grid3-load', 'grid3-loss', 'grid3-temp', 'season_x', 'month_x', 'week_x',
+       'weekday_x', 'holiday', 'hour_x',
+       'has incorrect data']]
 
-plt.plot(train['month_x'])
-plt.title("month")
-plt.savefig(savedir+"month_x")
-plt.show()
-plt.title("season")
-plt.plot(train['season_x'])
-plt.savefig(savedir+"season_x")
-plt.show()
-plt.title("weekday_x")
-plt.plot(train['weekday_x'])
-plt.savefig(savedir+"weekday_x")
-plt.show()
-plt.title("hour_x")
-plt.plot(train['hour_x'])
-plt.savefig(savedir+"hour_x")
-plt.show()
-plt.title("holiday")
-plt.plot(train['holiday'])
-plt.savefig(savedir+"holiday")
-plt.show()
 
-# only select the columns which we need
-df = train[['demand', 'grid1-load', 'grid1-loss', 'grid1-temp', 'grid2-load', 'grid2-loss', 'grid2_1-temp', 'grid2_2-temp', 'grid3-load', 'grid3-loss', 'grid3-temp']]
+# %% ---------------------- Descriptives before cleaning --------------------- #
+# Number of rows and columns
+print("Rows:              ", df.shape[0])
+print("Columns:           ", df.shape[1])
 
-#
+# Overview of numerical columns
+df.describe()
+df.describe().T.to_latex(savedir_meta + 'descriptives_raw.tex', float_format='%.2f')
+pd.DataFrame(df.shape[1]*[''], index=df.columns, columns=['Description']).to_latex(savedir_meta + 'features_raw.tex')
+
 print("Rows with NaN demand")
 print(df[df['demand'].isna()])
 
-print(df.describe())
-
 print(df.isnull().sum())
 
-"""
-sns.clustermap(df.corr())
-plt.show()
-print(len(np.fft.hfft(df['grid1-load'].to_numpy())))
-print(len(df['demand']))
+def evaluate_seasonaldata():
+    # season has 4 unique values
+    print(train['season_x'].unique())
+    # has 12 unique values
+    print(train['month_x'].unique())
+    # has
+    print(len(train['hour_x'].unique()))
 
-df.plot()
-plt.show()
-"""
+    plt.plot(train['month_x'])
+    plt.title("month")
+    plt.savefig(savedir_log+"month_x")
+    plt.show()
+    plt.title("season")
+    plt.plot(train['season_x'])
+    plt.savefig(savedir_log+"season_x")
+    plt.show()
+    plt.title("weekday_x")
+    plt.plot(train['weekday_x'])
+    plt.savefig(savedir_log+"weekday_x")
+    plt.show()
+    plt.title("hour_x")
+    plt.plot(train['hour_x'])
+    plt.savefig(savedir_log+"hour_x")
+    plt.show()
+    plt.title("holiday")
+    plt.plot(train['holiday'])
+    plt.savefig(savedir_log+"holiday")
+    plt.show()
+
+def print_corr_matrix():
+    sns.clustermap(df.corr())
+    plt.title("Correlation matrix")
+    plt.savefig(savedir_log+"corr_matrix")
+    plt.show()
